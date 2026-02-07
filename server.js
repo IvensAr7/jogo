72db60e365ea016c1599ef4855cb6d98195f3f3f
IvensAr7 <ivens.silva1@aluno.ce.gov.br>
2026-02-07 15:29:53 -0300

    Update 1

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 app.use(express.static("public"));
 
@@ -16,6 +19,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
+
